5918962cb1104d976ae45494722016878c77ce8f
Sun Howwrongbum <sun@libermatic.com>
2018-04-11 00:34:51 +0530

    Loan: filter queries and set default values for accounts

--- gwi_customization/microfinance/doctype/microfinance_loan/microfinance_loan.js
+++ gwi_customization/microfinance/doctype/microfinance_loan/microfinance_loan.js
@@ -2,5 +2,32 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Microfinance Loan', {
-  refresh: function(frm) {},
+  refresh: async function(frm) {
+    frm.fields_dict['loan_account'].get_query = doc => ({
+      filters: {
+        root_type: 'Asset',
+        is_group: false,
+      },
+    });
+    frm.fields_dict['interest_income_account'].get_query = doc => ({
+      filters: {
+        root_type: 'Income',
+        is_group: false,
+      },
+    });
+    if (frm.doc.__islocal) {
+      frm.set_df_property('loan_account', 'reqd', 1);
+      frm.set_df_property('interest_income_account', 'reqd', 1);
+      const { message: settings } = await frappe.db.get_value(
+        'Microfinance Loan Settings',
+        null,
+        ['loan_account', 'interest_income_account']
+      );
+      if (settings) {
+        const { loan_account, interest_income_account } = settings;
+        frm.set_value('loan_account', loan_account);
+        frm.set_value('interest_income_account', interest_income_account);
+      }
+    }
+  },
 });
