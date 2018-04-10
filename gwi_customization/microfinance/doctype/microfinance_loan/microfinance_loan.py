7a6270a0c64b9fe126d9a89a9862ef95732745aa
Sun Howwrongbum <sun@libermatic.com>
2018-04-11 00:23:01 +0530

    Loan: set default values on insert and submit

--- gwi_customization/microfinance/doctype/microfinance_loan/microfinance_loan.py
+++ gwi_customization/microfinance/doctype/microfinance_loan/microfinance_loan.py
@@ -5,7 +5,42 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import add_days, get_last_day
 
 
 class MicrofinanceLoan(Document):
-    pass
+    def before_save(self):
+        # set Loan Plan values
+        rate_of_interest, rate_of_late_charges, calculation_slab = \
+            frappe.get_value(
+                'Microfinance Loan Plan',
+                self.loan_plan,
+                [
+                    'rate_of_interest',
+                    'rate_of_late_charges',
+                    'calculation_slab',
+                ]
+            )
+        if not self.rate_of_interest:
+            self.rate_of_interest = rate_of_interest
+        if not self.rate_of_late_charges:
+            self.rate_of_late_charges = rate_of_late_charges
+        if not self.calculation_slab:
+            self.calculation_slab = calculation_slab
+
+        # set Loan Settings values
+        loan_account, interest_income_account = frappe.get_value(
+            'Microfinance Loan Settings',
+            None,
+            ['loan_account', 'interest_income_account']
+        )
+        if not self.loan_account:
+            self.loan_account = loan_account
+        if not self.interest_income_account:
+            self.interest_income_account = interest_income_account
+
+    def before_submit(self):
+        self.disbursement_status = 'Sanctioned'
+        self.recovery_status = 'Not Started'
+        # set to the first of the following month
+        self.billing_start_date = add_days(get_last_day(self.posting_date), 1)
--- gwi_customization/microfinance/doctype/microfinance_loan/test_microfinance_loan.py
+++ gwi_customization/microfinance/doctype/microfinance_loan/test_microfinance_loan.py
@@ -5,7 +5,74 @@ from __future__ import unicode_literals
 
 import frappe
 import unittest
+from gwi_customization.microfinance.doctype.microfinance_loanee.\
+    test_microfinance_loanee import create_test_loannee, remove_test_loanee
+
+test_dependencies = ['Microfinance Loan Plan']
 
 
 class TestMicrofinanceLoan(unittest.TestCase):
-    pass
+    def setUp(self):
+        create_test_loannee(customer_name='_Test Loanee 1')
+
+    def tearDown(self):
+        try:
+            loan = frappe.get_doc('Microfinance Loan', '_TEST LOAN')
+            if loan.docstatus == 1:
+                loan.cancel()
+        except frappe.DoesNotExistError:
+            pass
+        frappe.delete_doc_if_exists(
+            'Microfinance Loan', '_TEST LOAN', force=1
+        )
+        remove_test_loanee('_Test Loanee 1')
+
+    def test_statuses(self):
+        loan = create_test_loan()
+        self.assertEqual(loan.disbursement_status, 'Sanctioned')
+        self.assertEqual(loan.recovery_status, 'Not Started')
+
+    def test_billing_start_date(self):
+        loan = create_test_loan(posting_date='2017-11-11')
+        self.assertEqual(
+            loan.billing_start_date, frappe.utils.getdate('2017-12-01')
+        )
+
+    def test_loan_plan_values(self):
+        loan = create_test_loan(do_not_submit=True)
+        self.assertEqual(loan.rate_of_interest, 10.0)
+        self.assertEqual(loan.rate_of_late_charges, 5.0)
+        self.assertEqual(loan.calculation_slab, 10000.0)
+
+    def test_accounts(self):
+        loan_account, interest_income_account = frappe.get_value(
+            'Microfinance Loan Settings',
+            None,
+            ['loan_account', 'interest_income_account']
+        )
+        loan = create_test_loan(do_not_submit=True)
+        self.assertEqual(
+            loan.loan_account, loan_account
+        )
+        self.assertEqual(
+            loan.interest_income_account, interest_income_account
+        )
+
+
+def create_test_loan(**kwargs):
+    args = frappe._dict(kwargs)
+    doc = frappe.new_doc('Microfinance Loan')
+    doc.update({
+        'loan_no': args.loan_no or '_Test Loan',
+        'customer': args.customer or '_Test Loanee 1',
+        'posting_date': args.posting_date or '2017-08-19',
+        'company': args.company or '_Test Company',
+        'loan_plan': args.loan_plan or '_Test Loan Plan Basic',
+        'loan_principal': args.loan_principal or 100000.0,
+        'recovery_amount': args.recovery_amount or 2000.0,
+    })
+    if not args.do_not_insert:
+        doc.insert()
+        if not args.do_not_submit:
+            doc.submit()
+    return doc
