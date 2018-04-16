796a3b51cb2dd59ee77f9e40f620340bb1b0aa9d
Sun Howwrongbum <sun@libermatic.com>
2018-04-17 00:16:13 +0530

    Recovery: replace stub with proper methods and tests
    - remove unused fields
    - add period date fields to child table

--- gwi_customization/microfinance/doctype/microfinance_recovery/microfinance_recovery.py
+++ gwi_customization/microfinance/doctype/microfinance_recovery/microfinance_recovery.py
@@ -3,9 +3,153 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+from functools import reduce
 import frappe
-from frappe.model.document import Document
+from frappe.utils import flt
+from erpnext.controllers.accounts_controller import AccountsController
+from erpnext.accounts.general_ledger import make_gl_entries
+from erpnext.accounts.doctype.sales_invoice.sales_invoice \
+    import get_bank_cash_account
+from gwi_customization.microfinance.api.loan import get_outstanding_principal
+from gwi_customization.microfinance.api.interest import allocate_interests
 
 
-class MicrofinanceRecovery(Document):
-    pass
+class MicrofinanceRecovery(AccountsController):
+    def before_save(self):
+        self.total_interests = \
+            flt(self.total_amount) - flt(self.principal_amount)
+        self.total_charges = reduce(
+            lambda a, x: a + x.charge_amount, self.charges, 0
+        )
+        account_dict = get_bank_cash_account(
+            mode_of_payment=self.mode_of_payment or 'Cash',
+            company=self.company,
+        )
+        self.payment_account = account_dict.get('account')
+        self.periods = []
+        for period in allocate_interests(
+            self.loan, self.posting_date, self.total_interests
+        ):
+            self.append('periods', period)
+
+    def before_submit(self):
+        self.make_interests()
+
+    def on_submit(self):
+        self.make_gl_entries()
+        self.update_loan_status()
+
+    def before_cancel(self):
+        self.make_interests(cancel=1)
+
+    def on_cancel(self):
+        self.make_gl_entries(cancel=1)
+        self.update_loan_status()
+
+    def get_gl_dict(self, args):
+        gl_dict = frappe._dict({
+            'against_voucher_type': 'Microfinance Loan',
+            'against_voucher': self.loan,
+        })
+        gl_dict.update(args)
+        return super(MicrofinanceRecovery, self).get_gl_dict(gl_dict)
+
+    def make_gl_entries(self, cancel=0, adv_adj=0):
+        gl_entries = self.add_loan_gl_entries()
+        if self.periods:
+            gl_entries = self.add_interest_gl_entries(gl_entries)
+        if self.charges:
+            gl_entries = self.add_charges_gl_entries(gl_entries)
+        make_gl_entries(
+            gl_entries, cancel=cancel, adv_adj=adv_adj, merge_entries=False
+        )
+
+    def add_loan_gl_entries(self, gle=[]):
+        return gle + [
+            self.get_gl_dict({
+                'account': self.loan_account,
+                'credit': self.principal_amount,
+            }),
+            self.get_gl_dict({
+                'account': self.payment_account,
+                'debit': self.principal_amount,
+                'against': self.customer,
+                'remarks': 'Capital received',
+            })
+        ]
+
+    def add_interest_gl_entries(self, gle=[]):
+        return gle + [
+            self.get_gl_dict({
+                'account': self.loan_account,
+                'credit': self.total_interests,
+            }),
+            self.get_gl_dict({
+                'account': self.payment_account,
+                'debit': self.total_interests,
+                'against': self.customer,
+                'remarks': 'Interest received',
+            })
+        ]
+
+    def add_charges_gl_entries(self, gle=[]):
+        cost_center = frappe.db.get_value(
+            'Microfinance Loan Settings', None, 'cost_center'
+        )
+        return gle + map(
+            lambda row: self.get_gl_dict({
+                'account': row.charge_account,
+                'credit': row.charge_amount,
+                'cost_center': cost_center,
+                'remarks': row.charge,
+            }),
+            self.charges
+        ) + [
+            self.get_gl_dict({
+                'account': self.payment_account,
+                'debit': self.total_charges,
+                'against': self.customer,
+            })
+        ]
+
+    def make_interests(self, cancel=0):
+        if cancel:
+            for period in self.periods:
+                frappe.delete_doc_if_exists(
+                    'Microfinance Loan Interest', period.ref_interest
+                )
+        else:
+            for period in self.periods:
+                interest = frappe.get_doc({
+                    'doctype': 'Microfinance Loan Interest',
+                    'loan': self.loan,
+                    'posting_date': self.posting_date,
+                    'period': period.period_label,
+                    'start_date': period.start_date,
+                    'end_date': period.end_date,
+                    'billed_amount': period.billed_amount,
+                    'paid_amount': period.allocated_amount,
+                }).insert(ignore_if_duplicate=True)
+                period.update({'ref_interest': interest.name})
+
+    def update_loan_status(self):
+        """Method update recovery_status of Loan"""
+        loan = frappe.get_doc('Microfinance Loan', self.loan)
+        outstanding_principal = get_outstanding_principal(
+            self.loan, posting_date=self.posting_date
+        )
+        current_status = loan.recovery_status
+        current_clear = loan.clear_date
+        if outstanding_principal == 0 \
+                and loan.disbursement_status == 'Fully Disbursed':
+            loan.clear_date = self.posting_date
+            loan.recovery_status = 'Repaid'
+        else:
+            loan.clear_date = None
+            if outstanding_principal == loan.loan_principal:
+                loan.recovery_status = 'Not Started'
+            else:
+                loan.recovery_status = 'In Progress'
+        if loan.recovery_status != current_status \
+                or loan.clear_date != current_clear:
+            return loan.save()
--- gwi_customization/microfinance/doctype/microfinance_recovery/test_microfinance_recovery.py
+++ gwi_customization/microfinance/doctype/microfinance_recovery/test_microfinance_recovery.py
@@ -4,8 +4,239 @@
 from __future__ import unicode_literals
 
 import frappe
+from frappe.utils import getdate
 import unittest
+from gwi_customization.microfinance.doctype.microfinance_loan.\
+    test_microfinance_loan import create_test_loan, remove_test_loan
+from gwi_customization.microfinance.doctype.microfinance_disbursement.\
+    test_microfinance_disbursement import (
+        create_test_disbursement, remove_test_disbursement
+    )
+from gwi_customization.microfinance.utils import get_gle_by
+
+get_recovery_gle = get_gle_by('Microfinance Recovery')
 
 
 class TestMicrofinanceRecovery(unittest.TestCase):
-    pass
+    def tearDown(self):
+        remove_test_recovery()
+
+    def test_totals(self):
+        recovery = create_test_recovery(
+            total_amount=15000.0,
+            principal_amount=5000.0,
+            charges=[
+                {'charge': '_Test Charge 1', 'charge_amount': 150.0},
+                {'charge': '_Test Charge 2', 'charge_amount': 5000.0},
+            ]
+        )
+        self.assertEqual(recovery.total_interests, 10000.0)
+        self.assertEqual(recovery.total_charges, 5150.0)
+
+    def test_interests(self):
+        recovery = create_test_recovery(
+            posting_date='2017-09-17',
+            total_amount=15000.0,
+            principal_amount=5000.0,
+        )
+        self.assertEqual(len(recovery.periods), 1)
+        self.assertEqual(recovery.periods[0].billed_amount, 10000)
+        self.assertEqual(recovery.periods[0].outstanding_amount, 10000)
+        self.assertEqual(recovery.periods[0].allocated_amount, 10000)
+        period = frappe.get_doc(
+            'Microfinance Loan Interest', recovery.periods[0].ref_interest
+        ).as_dict()
+        to_check = {
+            'loan': '_Test Loan 1',
+            'posting_date': getdate('2017-09-17'),
+            'period': 'Aug, 2017',
+            'start_date': getdate('2017-08-19'),
+            'end_date': getdate('2017-08-31'),
+            'billed_amount': 10000,
+            'paid_amount': 10000,
+        }
+        for k, v in to_check.items():
+            self.assertEqual(v, period.get(k))
+
+    def test_interests_partial(self):
+        recovery = create_test_recovery(
+            total_amount=12000.0,
+            principal_amount=5000.0,
+        )
+        self.assertEqual(recovery.periods[0].billed_amount, 10000)
+        self.assertEqual(recovery.periods[0].outstanding_amount, 10000)
+        self.assertEqual(recovery.periods[0].allocated_amount, 7000)
+        period = frappe.get_doc(
+            'Microfinance Loan Interest', recovery.periods[0].ref_interest
+        )
+        to_check = {
+            'billed_amount': 10000,
+            'paid_amount': 7000,
+        }
+        for k, v in to_check.items():
+            self.assertEqual(v, period.get(k))
+
+    def test_interests_multiple(self):
+        recovery = create_test_recovery(
+            total_amount=22000.0,
+            principal_amount=5000.0,
+        )
+        periods = frappe.get_all(
+            'Microfinance Loan Interest', filters={'loan': '_Test Loan 1'}
+        )
+        self.assertEqual(len(periods), 2)
+        period = frappe.get_doc(
+            'Microfinance Loan Interest', recovery.periods[1].ref_interest
+        ).as_dict()
+        to_check = {
+            'loan': '_Test Loan 1',
+            'posting_date': getdate('2017-09-17'),
+            'period': 'Sep, 2017',
+            'start_date': getdate('2017-09-01'),
+            'end_date': getdate('2017-09-30'),
+            'billed_amount': 10000,
+            'paid_amount': 7000,
+        }
+        for k, v in to_check.items():
+            self.assertEqual(v, period.get(k))
+
+    def test_cancel_on_interests(self):
+        recovery = create_test_recovery(
+            total_amount=22000.0,
+            principal_amount=5000.0,
+        )
+        recovery.cancel()
+        periods = frappe.get_all(
+            'Microfinance Loan Interest', filters={'loan': '_Test Loan 1'}
+        )
+        self.assertEqual(len(periods), 0)
+
+    def test_updates_loan_status(self):
+        recovery = create_test_recovery()
+        recovery_status = frappe.get_value(
+            'Microfinance Loan', recovery.loan, 'recovery_status'
+        )
+        self.assertEqual(recovery_status, 'In Progress')
+
+    def test_updates_loan_status_for_repaid(self):
+        recovery = create_test_recovery(
+            principal_amount=100000
+        )
+        recovery_status = frappe.get_value(
+            'Microfinance Loan', recovery.loan, 'recovery_status'
+        )
+        self.assertEqual(recovery_status, 'Repaid')
+
+    def test_gle(self):
+        recovery = create_test_recovery(principal_amount=10000.0)
+        exp_gle = dict((d[0], d) for d in [
+            ['Microfinance Loans - _TC', 0, 10000, None],
+            ['Cash - _TC', 10000, 0, '_Test Loanee 1'],
+        ])
+        gl_entries = get_recovery_gle(recovery.name)
+        self.assertEqual(len(gl_entries), 2)
+        for gle in gl_entries:
+            self.assertEquals(exp_gle[gle.account][0], gle.account)
+            self.assertEquals(exp_gle[gle.account][1], gle.debit)
+            self.assertEquals(exp_gle[gle.account][2], gle.credit)
+            self.assertEquals(exp_gle[gle.account][3], gle.against)
+            self.assertEquals(recovery.loan, gle.against_voucher)
+
+    def test_cancel_on_gle(self):
+        recovery = create_test_recovery()
+        recovery.cancel()
+        gl_entries = get_recovery_gle(recovery.name)
+        self.assertEqual(len(gl_entries), 0)
+
+    def test_gle_with_charges(self):
+        recovery = create_test_recovery(
+            principal_amount=10000.0,
+            charges=[
+                {'charge': '_Test Charge 1', 'charge_amount': 150.0},
+                {'charge': '_Test Charge 2', 'charge_amount': 5000.0},
+            ]
+        )
+        exp_gle = dict((d[0], d) for d in [
+            ['Microfinance Loans - _TC', 0, 10000, None],
+            ['Service - _TC', 0, 5150, None],
+            ['Cash - _TC', 15150, 0, '_Test Loanee 1'],
+        ])
+        gl_entries = get_recovery_gle(recovery.name)
+        self.assertEqual(len(gl_entries), 3)
+        for gle in gl_entries:
+            self.assertEquals(exp_gle[gle.account][0], gle.account)
+            self.assertEquals(exp_gle[gle.account][1], gle.debit)
+            self.assertEquals(exp_gle[gle.account][2], gle.credit)
+            self.assertEquals(exp_gle[gle.account][3], gle.against)
+
+    def test_gle_with_interests(self):
+        recovery = create_test_recovery(
+            total_amount=15000.0,
+            principal_amount=5000.0,
+        )
+        exp_gle = dict((d[0], d) for d in [
+            ['Microfinance Loans - _TC', 0, 15000, None],
+            ['Cash - _TC', 15000, 0, '_Test Loanee 1'],
+        ])
+        gl_entries = get_recovery_gle(recovery.name)
+        self.assertEqual(len(gl_entries), 2)
+        for gle in gl_entries:
+            self.assertEquals(exp_gle[gle.account][0], gle.account)
+            self.assertEquals(exp_gle[gle.account][1], gle.debit)
+            self.assertEquals(exp_gle[gle.account][2], gle.credit)
+            self.assertEquals(exp_gle[gle.account][3], gle.against)
+
+
+def create_test_recovery(**kwargs):
+    args = frappe._dict(kwargs)
+    if not args.skip_dependencies:
+        create_test_loan(
+            loan_no=args.loan or '_Test Loan 1',
+            loan_plan=args.loan_plan or '_Test Loan Plan Basic',
+            loan_principal=args.loan_principal or 100000.0,
+        )
+        create_test_disbursement(
+            loan=args.loan or '_Test Loan 1',
+            amount=args.amount or args.loan_principal or 100000.0,
+            skip_dependencies=True,
+        )
+    doc = frappe.new_doc('Microfinance Recovery')
+    doc.update({
+        'loan': args.loan or '_Test Loan 1',
+        'posting_date': args.posting_date or '2017-09-17',
+        'total_amount': args.total_amount or args.principal_amount or 15000.0,
+        'principal_amount': args.principal_amount or 15000.0,
+        'mode_of_payment': args.mode_of_payment or 'Cash',
+    })
+    if args.charges:
+        for item in args.charges:
+            doc.append('charges', item)
+    if args.mode_of_payment == 'Cheque':
+        doc.update({
+            'cheque_no': args.cheque_no or 'ABCABC',
+            'cheque_date': args.cheque_date or '2017-08-20',
+        })
+    if not args.do_not_insert:
+        doc.insert(ignore_if_duplicate=True)
+        if not args.do_not_submit:
+            doc.submit()
+    return doc
+
+
+def remove_test_recovery(loan='_Test Loan 1', keep_dependencies=False):
+    recoveries = frappe.get_all(
+        'Microfinance Recovery', filters=[['loan', 'in', loan]],
+    )
+    for doc in recoveries:
+        try:
+            rec = frappe.get_doc('Microfinance Recovery', doc.name)
+            if rec.docstatus == 1:
+                rec.cancel()
+        except frappe.DoesNotExistError:
+            pass
+        frappe.delete_doc(
+            doctype='Microfinance Recovery', name=doc.name, force=True
+        )
+    remove_test_disbursement(loan, keep_dependencies=keep_dependencies)
+    if not keep_dependencies:
+        remove_test_loan(loan)
