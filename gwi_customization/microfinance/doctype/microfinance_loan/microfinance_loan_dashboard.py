483d9ec54ee7e81e3d7d1931574412be009f03ba
Sun Howwrongbum <sun@libermatic.com>
2018-04-17 03:05:04 +0530

    Loan: add dashboard

---
+++ gwi_customization/microfinance/doctype/microfinance_loan/microfinance_loan_dashboard.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Libermatic and contributors
+# For license information, please see license.txt
+
+from frappe import _
+
+
+def get_data():
+    return {
+        'fieldname': 'loan',
+        'transactions': [
+            {
+                'label': _('Payments'),
+                'items': ['Microfinance Disbursement', 'Microfinance Recovery']
+            },
+        ]
+    }
