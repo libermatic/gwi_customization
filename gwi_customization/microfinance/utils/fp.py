b073834215fb876cf3881dd297a6acdb23c7df2c
Sun Howwrongbum <sun@libermatic.com>
2018-04-18 23:51:24 +0530

    Utils: create fp functions

---
+++ gwi_customization/microfinance/utils/fp.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, Libermatic and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+from functools import reduce
+from copy import deepcopy
+
+
+def compose(*funcs):
+    return reduce(lambda f, g: lambda x: f(g(x)), funcs, lambda x: x)
+
+
+def update(kv_dict):
+    def fn(item):
+        new_item = deepcopy(item)
+        new_item.update(kv_dict)
+        return new_item
+    return fn
