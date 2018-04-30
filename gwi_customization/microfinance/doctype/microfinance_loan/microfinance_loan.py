dd8038eac5a8aa7cf1b15022708c6aae37ac8414
Sun Howwrongbum <sun@libermatic.com>
2018-04-30 17:58:07 +0530

    Loan: add validation for loan amount and duration

--- gwi_customization/microfinance/doctype/microfinance_loan/microfinance_loan.py
+++ gwi_customization/microfinance/doctype/microfinance_loan/microfinance_loan.py
@@ -5,10 +5,15 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from frappe.utils import add_days, get_last_day
+from frappe.utils import add_days, get_last_day, getdate, flt, fmt_money
 from frappe.contacts.doctype.address.address import get_default_address
-from gwi_customization.microfinance.utils.fp import join
-from gwi_customization.microfinance.api.loan import get_chart_data
+from functools import reduce, partial
+from gwi_customization.microfinance.utils.fp import join, pick
+from gwi_customization.microfinance.api.loan import (
+    get_chart_data,
+    calculate_principal,
+    get_outstanding_principal,
+)
 
 
 def _make_address_text(customer=None):
@@ -26,7 +31,86 @@ def _make_address_text(customer=None):
     return join(', ')([address.get('address_line1'), address.get('city')])
 
 
+def _fmt_money(amount):
+    return fmt_money(
+        amount,
+        precision=0,
+        currency=frappe.defaults.get_user_default('currency'),
+    )
+
+
+def _existing_loans_by(customer):
+    return frappe.get_all(
+        'Microfinance Loan',
+        filters=[
+            ['docstatus', '=', '1'],
+            ['customer', '=', customer],
+            ['recovery_status', 'in', 'Not Started, In Progress'],
+        ]
+    )
+
+
+def _reduce_outstanding_by(posting_date):
+    def fn(a, x):
+        return a + partial(
+            get_outstanding_principal, posting_date=posting_date
+        )(x)
+    return fn
+
+
 class MicrofinanceLoan(Document):
+    def validate(self):
+        if self.recovery_amount > self.loan_principal:
+            frappe.throw("Recovery Amount cannot exceed Principal.")
+        self.validate_allowable_amount()
+
+    def validate_allowable_amount(self):
+        effective_date = frappe.get_value(
+            'Loan Plan', self.loan_plan, 'effective_from'
+        )
+        if effective_date and \
+                getdate(effective_date) > getdate(self.posting_date):
+            return None
+
+        date_of_retirement, net_salary_amount = frappe.get_value(
+            'Microfinance Loanee',
+            {'customer': self.customer},
+            ['date_of_retirement', 'net_salary_amount']
+        )
+        if not date_of_retirement or not net_salary_amount:
+            return None
+        allowed = calculate_principal(
+            income=net_salary_amount,
+            loan_plan=self.loan_plan,
+            end_date=date_of_retirement,
+            execution_date=self.posting_date,
+        )
+        if self.loan_principal > flt(allowed.get('principal')):
+            frappe.throw(
+                "Requested principal cannot exceed {}".format(
+                    _fmt_money(allowed.get('principal'))
+                )
+            )
+        if self.recovery_amount \
+                < self.loan_principal / allowed.get('duration'):
+            frappe.throw(
+                "Recovery Amount cannot be less than {}".format(
+                    _fmt_money(self.loan_principal / allowed.get('duration'))
+                )
+            )
+        tentative_outstanding = reduce(
+            _reduce_outstanding_by(self.posting_date),
+            map(pick('name'), _existing_loans_by(self.customer)),
+            self.loan_principal
+        )
+        if tentative_outstanding > flt(allowed.get('principal')):
+            frappe.throw(
+                "Customer already has existing loans. "
+                "Total principal would exceed allowed {}".format(
+                    _fmt_money(allowed.get('principal')),
+                )
+            )
+
     def onload(self):
         self.set_onload(
             'address_text', _make_address_text(self.customer)
--- gwi_customization/microfinance/doctype/microfinance_loan/test_microfinance_loan.py
+++ gwi_customization/microfinance/doctype/microfinance_loan/test_microfinance_loan.py
@@ -7,6 +7,9 @@ import frappe
 import unittest
 from gwi_customization.microfinance.doctype.microfinance_loanee.\
     test_microfinance_loanee import create_test_loanee, remove_test_loanee
+# from gwi_customization.microfinance.doctype.microfinance_disbursement.\
+#     test_microfinance_disbursement \
+#     import create_test_disbursement, remove_test_disbursement
 
 test_dependencies = ['Microfinance Loan Plan']
 
@@ -46,6 +49,75 @@ class TestMicrofinanceLoan(unittest.TestCase):
             loan.interest_income_account, interest_income_account
         )
 
+    def test_raises_when_recovery_more_than_principal(self):
+        with self.assertRaises(frappe.exceptions.ValidationError):
+            create_test_loan(
+                loan_principal=10000.0,
+                recovery_amount=10001.0,
+            )
+
+    def test_principal_more_than_allowed(self):
+        create_test_loanee(
+            customer_name='_Test Loanee 1',
+            date_of_retirement='2018-12-12',
+            net_salary_amount=3000.0,
+        )
+        with self.assertRaises(frappe.exceptions.ValidationError):
+            create_test_loan(
+                skip_dependencies=True,
+                posting_date='2017-12-01',
+                loan_principal=11001.0,
+                recovery_amount=1500.0,
+            )
+
+    def test_recovery_less_than_allowed(self):
+        create_test_loanee(
+            customer_name='_Test Loanee 1',
+            date_of_retirement='2018-12-12',
+            net_salary_amount=3000.0,
+        )
+        with self.assertRaises(frappe.exceptions.ValidationError):
+            create_test_loan(
+                skip_dependencies=True,
+                posting_date='2017-12-01',
+                loan_principal=11000.0,
+                recovery_amount=999.0,
+            )
+
+    def test_total_outstanding_more_than_allowed(self):
+        create_test_loanee(
+            customer_name='_Test Loanee 1',
+            date_of_retirement='2018-12-12',
+            net_salary_amount=3000.0,
+        )
+        create_test_loan(
+            skip_dependencies=True,
+            posting_date='2017-12-01',
+            loan_principal=6000.0,
+            recovery_amount=600.0,
+        )
+        disbursement = frappe.get_doc({
+            'doctype': 'Microfinance Disbursement',
+            'loan': '_TEST LOAN',
+            'posting_date': '2017-12-02',
+            'amount': 6000.0,
+            'mode_of_payment': 'Cash',
+        })
+        disbursement.insert()
+        disbursement.submit()
+        with self.assertRaises(frappe.exceptions.ValidationError):
+            create_test_loan(
+                skip_dependencies=True,
+                loan_no='_Test Loan Another',
+                posting_date='2018-02-12',
+                loan_principal=6000.0,
+                recovery_amount=700.0,
+            )
+        disbursement.cancel()
+        frappe.delete_doc(
+            'Microfinance Disbursement', name=disbursement.name, force=True
+        )
+
 
 def create_test_loan(**kwargs):
     args = frappe._dict(kwargs)
@@ -68,7 +140,7 @@ def create_test_loan(**kwargs):
     return doc
 
 
-def remove_test_loan(loan='_TEST LOAN', keep_dependencies=False):
+def remove_test_loan(loan='_Test Loan', keep_dependencies=False):
     try:
         doc = frappe.get_doc('Microfinance Loan', loan)
         if doc.docstatus == 1:
