0f809f71be6c3fc843f2adca7692aafa16f93bab
Sun Howwrongbum <sun@libermatic.com>
2020-03-15 17:44:30 +0530

    refactor: Recovery js to bundle

--- gwi_customization/public/js/scripts/index.js
+++ gwi_customization/public/js/scripts/index.js
@@ -1,2 +1,3 @@
 export { default as audit_statement } from './audit_statement';
 export { default as microfinance_loan } from './microfinance_loan';
+export { default as microfinance_recovery } from './microfinance_recovery';
